a821e97b290208acdcac1252fe8d30c5188c2ad6
toluwaanimi <emmanueltolu.adebayo@gmail.com>
2021-03-18 06:38:59 +0100

    feat(filereaderexample): added example implementing how to read from files

---
+++ src/FileReaderExample.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderExample {
+    public static void main(String[] args) {
+        String fileName = "cit417test.txt";
+        String line =null;
+        try{
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader =new BufferedReader(fileReader);
+            while ((line =bufferedReader.readLine())!= null){
+                System.out.println(line);
+            }
+
+        }catch (FileNotFoundException fileNotFoundException){
+            System.out.println("Unable to Open the file: "+fileName);
+        } catch (IOException e) {
+            System.out.println("Error in reading File: "+fileName);
+        }
+
+    }
+
+}
