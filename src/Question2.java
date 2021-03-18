ee404cb0b42ecdcf898d155fcdab4d5050790505
toluwaanimi <emmanueltolu.adebayo@gmail.com>
2021-03-18 08:36:50 +0100

    feat(question2): fixed array issues

--- src/Question2.java
+++ src/Question2.java
@@ -5,20 +5,18 @@ import java.util.List;
 public class Question2 {
 
     public static void main(String[] args) {
-        double[] array = {1.1, 3.4, 6, 3.1};
-        averageOfAll(array);
+        averageOfAll();
     }
 
     /**
      * @description calculate the average of the numbers divided by 6
-     * @param array
      */
-    public static void averageOfAll(double[] array) {
-        ArrayList<Double> arrayList = new ArrayList<>();
+    public static void averageOfAll() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
         double sum = 0;
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] % 6 == 0) {
-                arrayList.add(array[i]);
+        for (int i = 0; i < 2000; i++) {
+            if (i % 6 == 0) {
+                arrayList.add(i);
             }
         }
         for (int i = 0; i < arrayList.size(); i++){
