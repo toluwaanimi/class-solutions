73c995d36b0d8b76e73ffab31e14f48f56722ae2
toluwaanimi <emmanueltolu.adebayo@gmail.com>
2021-03-18 06:26:34 +0100

    feat(Question1):added usecase for manual sorting

--- src/Question1.java
+++ src/Question1.java
@@ -9,11 +9,25 @@ public class Question1 {
     public static void main(String[] args) {
         double[] array = {1.1, 3.4, 6.2, 3.1};
         Question1 question1 = new Question1();
-        question1.biggestNumber(array);
+        question1.biggestNumberBySorting(array);
     }
 
-    public void biggestNumber(double[] array) {
+    public void biggestNumberUsingDotSort(double[] array) {
         Arrays.sort(array);
+        System.out.println(array[array.length - 1]);
+    }
+
+    public void biggestNumberBySorting(double[] array){
+        double temp;
+        for (int i =1; i < array.length; i++){
+            for (int j = i; j > 0; j--){
+                if (array[j] < array[j-1]){
+                    temp = array[j];
+                    array[j] = array[j -1];
+                    array[j-1] = temp;
+                }
+            }
+        }
         System.out.println(array[array.length - 1]);
     }
 
